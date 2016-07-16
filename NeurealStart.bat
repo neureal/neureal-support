25630968919e7de3a888f5d3b9a57bf80f220df0
Wil Bown <wilbown@gmail.com>
2016-07-16 17:59:57 -0600

    - initial commit

---
+++ NeurealStart.bat
@@ -0,0 +1,2 @@
+START noocoind.exe -debug -printtoconsole -testnet -server -connect=174.52.202.72 -rpcuser=noocoinrpc -rpcpassword=sskvik3290f87uvkk2sovllshj390gf876fdSGkza1
+START Noo-1.0-SNAPSHOT.jar
